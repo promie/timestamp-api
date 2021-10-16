252420cd3bd95027032e7cca9cb1b4024c1aa081
Promie Yutasane <promie.yutasane@gmail.com>
2021-10-17 10:46:10 +1100

    Updated test cases. all tests passing.

--- src/services/test/TimestampService.unit.test.ts
+++ src/services/test/TimestampService.unit.test.ts
@@ -2,17 +2,24 @@ import TimestampService from '../TimestampService'
 
 describe('TimestampService', () => {
   describe('#getTimestampByDate', () => {
-    it(
-      'returns an object that contains the correct the unix and utc values ' +
-        'if a correct unix time is passed in',
-      () => {
+    describe('timestamp in milliseconds', () => {
+      it('returns an object that contains the correct unix and utc values - Fri, 25 Dec 2015', () => {
         const date = '1451001600000'
 
         expect(TimestampService.getTimestampByDate(date)).toEqual({
           unix: 1451001600000,
           utc: 'Fri, 25 Dec 2015 00:00:00 GMT',
         })
-      },
-    )
+      })
+
+      it('returns an object that contains the correct unix and utc values - Thu, 07 Aug 1986', () => {
+        const date = '523756800000'
+
+        expect(TimestampService.getTimestampByDate(date)).toEqual({
+          unix: 523756800000,
+          utc: 'Thu, 07 Aug 1986 00:00:00 GMT',
+        })
+      })
+    })
   })
 })
