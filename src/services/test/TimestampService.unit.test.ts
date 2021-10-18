6c733832620e0e077e3f0f7308926cb4e2942ba3
Promie Yutasane <promie.yutasane@gmail.com>
2021-10-19 06:41:21 +1100

    unit test for getCurrentTimestamp function

--- src/services/test/TimestampService.unit.test.ts
+++ src/services/test/TimestampService.unit.test.ts
@@ -1,6 +1,23 @@
 import TimestampService from '../TimestampService'
 
 describe('TimestampService', () => {
+  describe('#getCurrentTimestamp', () => {
+    it('returns an object that contains the unix in (milliseconds) and utc values of the current (mock) date', () => {
+      const mockDate = new Date(1451001600000)
+      // @ts-ignore
+      const spy = jest.spyOn(global, 'Date').mockImplementation(() => mockDate)
+
+      const date = new Date()
+
+      expect(TimestampService.getTimestampByDate(date.toString())).toEqual({
+        unix: 1451001600000,
+        utc: 'Fri, 25 Dec 2015 00:00:00 GMT',
+      })
+
+      spy.mockRestore()
+    })
+  })
+
   describe('#getTimestampByDate', () => {
     describe('timestamp in milliseconds', () => {
       it('returns an object that contains the correct unix in (milliseconds) and utc values - Fri, 25 Dec 2015', () => {
