92e0536555b890cb32c0de5b076ab2ce28710a72
Promie Yutasane <promie.yutasane@gmail.com>
2021-10-19 07:27:59 +1100

    documentation on the service levels for the two functions

--- src/services/TimestampService.ts
+++ src/services/TimestampService.ts
@@ -1,6 +1,12 @@
 import { isNumber } from '../helpers'
 import { ITimestamp } from '../types/timestamp'
 
+/**
+ *  Returning the current timestamp
+ *  for the unix and utc time
+ *
+ * @returns ITimestamp
+ */
 const getCurrentTimestamp = (): ITimestamp => {
   const date = new Date()
 
@@ -9,6 +15,15 @@ const getCurrentTimestamp = (): ITimestamp => {
     utc: date.toUTCString(),
   }
 }
+
+/**
+ *  Returning the current time
+ *  for the unix and utc time
+ *  based on the users dateInput
+ *
+ * @param {string} dateInput
+ * @returns ITimestamp
+ */
 const getTimestampByDate = (dateInput: string): ITimestamp => {
   let dateParam: string | number
   let date: Date
